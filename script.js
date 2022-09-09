49b58974e81476e2a0f59f8d17432a970c01e75c
TierneySalzmann <tierneysalzmann@gmail.com>
2022-09-09 17:09:25 -0400

    Added input event listener for color1 and color2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
-console.log(css);
-console.log(color1);
-console.log(color2);
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+}
+
+//Do not need to call function as input event is triggered automatically and function runs automatically with event
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
